5a6a48dda0338168b8b15c5d9ca0c4deec27a680
timothy <tdsfee@gmail.com>
2023-09-20 21:36:09 -0500

    Added rolling stats.

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 import json
 import random
+import time
 
 with open("D&D.json", "r") as f:
     data =json.load(f)
@@ -12,4 +13,20 @@ raceName = data['race'][randIDRace]['name']
 
 className = data['class'][randIDClass]['name']
 
+strength = random.randint(7,20)
+constitution = random.randint(7,20)
+dexterity = random.randint(7,20)
+intelligence = random.randint(7,20)
+wisdom = random.randint(7,20)
+charisma = random.randint(7,20)
+
 print(f"Race: {raceName} \nClass: {className}")
+
+print("Rolling your stats..")
+time.sleep(2)
+print(f"Strength: {strength}")
+print(f"Constitution: {constitution}")
+print(f"Dexterity: {dexterity}")
+print(f"Intelligence: {intelligence}")
+print(f"Wisdom: {wisdom}")
+print(f"Charisma: {charisma}")
