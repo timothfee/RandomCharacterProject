120caad5c0efe1c47eccf163a8130e27ce9cef38
timothy <tdsfee@gmail.com>
2023-09-25 10:28:41 -0500

    Added stats list

--- random_character.py
+++ random_character.py
@@ -3,34 +3,45 @@ import random
 import time
 import os
 
-
 with open("D&D.json", "r") as f:
-    data =json.load(f)
+    data = json.load(f)
 
 clear = lambda: os.system('cls')
 
-rand_id_race = random.randint(0,4)
+rand_id_race = random.randint(0, 4)
 
-rand_id_class = random.randint(0,9)
+rand_id_class = random.randint(0, 9)
 
 race_name = data['race'][rand_id_race]['name']
 
 class_name = data['class'][rand_id_class]['name']
 
-stat1 = random.randint(7,20)
-stat2 = random.randint(7,20)
-stat3 = random.randint(7,20)
-stat4 = random.randint(7,20)
-stat5 = random.randint(7,20)
-stat6 = random.randint(7,20)
+sub_class = data['class'][rand_id_class]['sub-class']
+
+subclass_len = len(sub_class)
+# Need to figure out how to pull length of subclass list from JSON.
+print(subclass_len)
+
+rand_id_subclass = random.randint(0, subclass_len)
+
+subclass_name = data['class'][rand_id_class]['sub-class'][rand_id_subclass]
+
+stat1 = random.randint(7, 20)
+stat2 = random.randint(7, 20)
+stat3 = random.randint(7, 20)
+stat4 = random.randint(7, 20)
+stat5 = random.randint(7, 20)
+stat6 = random.randint(7, 20)
 
 stats = [stat1, stat2, stat3, stat4, stat5, stat6]
 
-print(f"Race: {race_name} \nClass: {class_name}")
+print()
 
-for x in range (0,5):  
+print(f"Race: {race_name} \nClass: {class_name}\nSub-Class: {subclass_name}")
+
+for x in range(0, 5):
     b = "Rolling stats" + "." * x
-    print (b, end="\r")
+    print(b, end="\r")
     time.sleep(1)
 print(f"\nStat 1: {stat1}")
 print(f"Stat 2: {stat2}")
@@ -46,9 +57,9 @@ intelligence = input("Intelligence: ")
 wisdom = input("Wisdom: ")
 charisma = input("Charisma: ")
 name = input("Good job! Now give your character a name:\n")
-for x in range (0,5):  
+for x in range(0, 5):
     b = "Thank you! Generating character sheet" + "." * x
-    print (b, end="\r")
+    print(b, end="\r")
     time.sleep(1)
 clear()
 
